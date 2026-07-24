060fb1838b57e9fe211c86dc49581d68f874db29
AntonVelev21 <velevanton80@gmail.com>
2026-07-24 16:10:36 +0300

    Resolved first NeetCode issue

---
+++ Arrays & Hashing/217_contains_duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: list[int]) -> bool:
+        reviews_nums = {}
+        for n in nums:
+            if n in reviews_nums:
+                return True
+            reviews_nums[n] = True
+
+        return False
+
+
